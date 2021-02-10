6900b8f6f85cefc373b6ab116c0f4df29b32aefe
nair-siddharth <70758206+nair-siddharth@users.noreply.github.com>
2021-02-10 21:03:34 +0300

    Added logo

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './Header.css';
+import logo from '../../assets/logo.svg';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -7,9 +8,12 @@ import Button from '@material-ui/core/Button';
 class Header extends Component{
     render(){
         return (
-            <div id = 'Header'>
-                <Button color = "default" variant = "contained" onClick = {(e) => alert("Login Successful")}>Login</Button>
-                {/* <Button variant="contained" color="secondary">Delete</Button> */}
+            <div>
+                <header className = 'Header'>
+                    <img src = {logo} className = 'appLogo' alt = 'play'></img>
+                    <Button color = "default" variant = "contained" onClick = {(e) => alert("Login Successful")}>Login</Button>
+                    {/* <Button variant="contained" color="secondary">Delete</Button> */}
+                </header>
             </div>
         )
     }
